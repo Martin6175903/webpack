b68828542576111a672ee97d5f026efc5ba7ecd1
Martin6175903 <martin6175903@yandex.ru>
2023-06-15 12:02:32 +0300

    added babel, lodash and dinamycs imports for Webpack

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,34 @@ import Post from "@models/Post";
 // import xml from './assets/data.xml';
 // import csv from './assets/data.csv';
 import WebpackLogo from "./assets/webpack-logo.png";
+import React from "react";
+import {render} from "react-dom";
+import './babel';
 import css from './styles/styles.css';
 import './styles/index.scss';
 
 const post = new Post('Webpack post title', WebpackLogo);
 
-$('pre').addClass('code').html(post.toString());
-
 console.log('Post to String: ', post.toString());
+
+const App = () => (
+        <div className="container">
+            <h1>Webpack complied!</h1>
+
+            <hr />
+            <div className="logo" />
+
+            <hr />
+
+            <pre></pre>
+            <hr />
+            <div className="box">
+                <h2>SCSS</h2>
+            </div>
+        </div>
+)
+
+render(<App/>, document.getElementById('app'));
 
 // console.log("JSON: ", json);
 //
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const {plugins} = require("@babel/preset-env/lib/plugins-compat-data");
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -13,8 +15,9 @@ console.log(isDev);
 const optimization = () => {
     const config = {
         splitChunks: {
-            chunks: "all"
-        }
+            chunks: 'all'
+        },
+        runtimeChunk: 'single'
     };
 
     if (isProd) {
@@ -47,30 +50,38 @@ const cssLoaders = extra => {
     return loaders;
 }
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: "development",
-    entry: {
-        main: './index.js',
-        analytics: './analytics.js'
-    },
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js', '.json'],
-        alias: {
-            "@models": path.resolve(__dirname, 'src/models'),
-            "@": path.resolve(__dirname, 'src')
-        }
-    },
-    optimization: optimization(),
-    devServer: {
-        port: 3100,
-        hot: isDev
-    },
-    plugins: [
+const babelOptions = preset => {
+    const opts = {
+        presets: [
+            '@babel/preset-env'
+        ],
+        plugins: [
+            '@babel/plugin-proposal-class-properties'
+        ]
+    }
+
+    if (preset) {
+        opts.presets.push(preset);
+    }
+
+    return opts;
+}
+
+const jsLoaders = () => {
+    const loaders =  [{
+        loader: 'babel-loader',
+        options: babelOptions()
+    }];
+
+    if (isDev) {
+        loaders.push('eslint-loader');
+    }
+
+    return loaders;
+}
+
+const pluginsAll = () => {
+    const base = [
         // Загрузка шаблона html, который нам нужен
         new HtmlWebpackPlugin({
             template: "./index.html",
@@ -91,7 +102,40 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: filename('css')
         })
-    ],
+    ];
+
+    if (isProd) {
+        base.push(new BundleAnalyzerPlugin());
+    }
+
+    return base;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: {
+        main: ['@babel/polyfill', './index.jsx'],
+        analytics: './analytics.ts'
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json'],
+        alias: {
+            "@models": path.resolve(__dirname, 'src/models'),
+            "@": path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 3100,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: pluginsAll(),
     module: {
         rules: [
             {
@@ -117,6 +161,23 @@ module.exports = {
             {
                 test: /\.csv$/,
                 use: 'csv-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: jsLoaders()
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: babelOptions('@babel/preset-typescript')
+            },
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: babelOptions('@babel/preset-react')
             }
         ]
     }
